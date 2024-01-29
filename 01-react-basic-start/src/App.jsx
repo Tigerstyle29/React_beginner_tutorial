a582c80734b71ab8149bea7d16cbe3cef4d953fe
Tigran Vartanyan <tvartanyan29@yandex.ru>
2024-01-30 01:02:01 +0400

    React component separation

--- 01-react-basic-start/src/App.jsx
+++ 01-react-basic-start/src/App.jsx
@@ -1,75 +1,14 @@
 import Header from './components/Header'
-import WayToTeach from './components/WayToTeach'
-import Button from './components/Button/Button'
-import { useState } from 'react'
-import { ways, differences } from './data'
+import TeachingSection from './components/TeachingSection'
+import DifferencesSection from './components/DifferencesSection'
 
 function App() {
-  const [ contentType, setContentType] = useState(null)
-
-  function handleClick(type) {
-    setContentType(type)
-  }
-
-  // let tabContent = null
-
-  // if (contentType) {
-  //   tabContent = <p>{differences[contentType]}</p>
-  // } else {
-  //   tabContent = <p>Нажми на кнопку</p>
-  // }
-
-
   return (
     <div>
       <Header />
       <main>
-        <section>
-          <h3>Наш подход к обучению</h3>
-
-          <ul>
-            {ways.map((way) => (
-              <WayToTeach key={way.title} {...way} />
-            ))}
-          </ul>
-        </section>
-        <section>
-          <h3>Чем мы отличаемся от других</h3>
-          
-          <Button 
-            isActive={contentType === 'way'}
-            onClick={() => handleClick('way')}
-          >
-            Подход
-          </Button>
-          <Button 
-            isActive={contentType === 'easy'}
-            onClick={() => handleClick('easy')}
-          >
-            Доступность
-          </Button>
-          <Button
-            isActive={contentType === 'program'} 
-            onClick={() => handleClick('program')}
-          >
-            Концентрация
-          </Button>   
-
-          {/* {contentType ? (
-            <p>{differences[contentType]}</p>
-          ) : (
-            <p>Нажми на кнопку</p>
-          )}    */}
-
-          {/* {!contentType ? <p>Нажми на кнопку</p> : null}
-          {contentType ? <p>{differences[contentType]}</p> : null} */}
-
-          {!contentType && <p>Нажми на кнопку</p>}
-
-          {contentType && <p>{differences[contentType]}</p>}
-
-          {/* {tabContent} */}
-        </section>
+        <TeachingSection />
+        <DifferencesSection />
       </main>
     </div>
   )
