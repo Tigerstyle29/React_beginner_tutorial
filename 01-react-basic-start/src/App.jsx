6e1c5dea2e067de879024bf67915337f7be3fedd
Tigran Vartanyan <tvartanyan29@yandex.ru>
2024-01-31 18:21:06 +0400

    JSX (how work)

--- 01-react-basic-start/src/App.jsx
+++ 01-react-basic-start/src/App.jsx
@@ -1,16 +1,18 @@
 import Header from './components/Header'
 import TeachingSection from './components/TeachingSection'
 import DifferencesSection from './components/DifferencesSection'
+import IntroSection from './components/introSection'
 
 function App() {
   return (
-    <div>
+    <>
       <Header />
       <main>
+        <IntroSection />
         <TeachingSection />
         <DifferencesSection />
       </main>
-    </div>
+    </>
   )
 }
 
