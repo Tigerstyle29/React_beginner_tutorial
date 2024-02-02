20fdf1e5746befc9fa87f32b0a8b6d2dfd6ac743
Tigran Vartanyan <tvartanyan29@yandex.ru>
2024-02-02 15:38:11 +0400

    Styled-components

--- 01-react-basic-start/src/components/Button/Button.jsx
+++ 01-react-basic-start/src/components/Button/Button.jsx
@@ -1,10 +1,10 @@
-import './Button.css'
+import classes from './Button.module.css'
 
 export default function Button ({ children, onClick, isActive }) {
-    // let classes = 'button'
-    // if (isActive) classes += ' Active'
     return ( 
-        <button className={isActive ? 'button active' : 'button'} onClick={onClick}>
+        <button className={isActive ? `${classes.button} ${classes.active}` : classes.button} 
+            onClick={onClick}
+        >
             {children}
         </button>
     )
---
+++ 01-react-basic-start/src/components/Header/Header.jsx
@@ -0,0 +1,30 @@
+import { useState } from 'react'
+import logo from '/logo-name.svg'
+import { styled } from 'styled-components'
+// import './Header.css'
+
+const HeaderContainer = styled.header`
+    height: 50px;
+    display: flex;
+    padding: 0 2rem;
+    justify-content: space-between;
+    align-items: center;
+    border-bottom: 1px solid #ccc;
+    background: #fafafa;
+`
+
+function Header() {
+  const [now, setNow] = useState(new Date())
+
+  setInterval(() => setNow(new Date()), 1000) 
+  
+    return(
+      <HeaderContainer>
+        <img src={logo} alt={'Result'} />
+  
+        <span>Время сейчас: {now.toLocaleTimeString()}</span>
+      </HeaderContainer>
+    )
+  }
+
+export default Header
