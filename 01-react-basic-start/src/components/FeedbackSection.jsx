5bfc97dec2cf71b638f27b852562d19279af1d07
Tigran Vartanyan <tvartanyan29@yandex.ru>
2024-02-08 16:52:37 +0400

    useRef

--- 01-react-basic-start/src/components/FeedbackSection.jsx
+++ 01-react-basic-start/src/components/FeedbackSection.jsx
@@ -1,10 +1,33 @@
-import { useState } from 'react'
+import { useState, useRef } from 'react'
 import Button from './Button/Button'
+
+function StateVsRef() {
+    const input = useRef()
+    const [show, setShow] = useState(false)
+
+    function handleKeyDown(event) {
+        if (event.key === 'Enter') {
+            setShow(true)
+        }
+    }
+
+    return (
+       <div>
+            <h3>Input value: {show && input.current.value}</h3>
+            <input
+                ref={input}
+                type="text" 
+                onKeyDown={handleKeyDown}
+                className="control" 
+            />
+       </div> 
+    )
+}
 
 export default function FeedBackSection() {
     const [form, setForm] = useState({
         name: '',
-        hasError: true,
+        hasError: false,
         reason: 'help'
     })
     // const [name, setName] = useState('')
@@ -31,7 +54,7 @@ export default function FeedBackSection() {
 
             {/* <Button onClick={toggleError}>Toggle Error</Button> */}
 
-            <form>
+            <form style={{ marginBottom: '1rem'}}>
                 <label htmlFor="name">Ваше имя</label>
                 <input 
                     type="text" 
@@ -58,12 +81,13 @@ export default function FeedBackSection() {
                     <option value="suggest">Предложение</option>
                 </select>
 
-                <pre>{JSON.stringify(form, null, 2)}</pre>
+                {/* <pre>{JSON.stringify(form, null, 2)}</pre> */}
 
                 <Button disabled={form.hasError} isActive={!form.hasError}>
                     Отправить
                 </Button>
             </form>
+            <StateVsRef />
         </section>
     )
 }
