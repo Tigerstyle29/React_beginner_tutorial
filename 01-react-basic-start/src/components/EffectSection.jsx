c68ae0b171417508299c2b963a2d32efaa702733
Tigran Vartanyan <tvartanyan29@yandex.ru>
2024-02-23 01:04:43 +0400

    First hook

--- 01-react-basic-start/src/components/EffectSection.jsx
+++ 01-react-basic-start/src/components/EffectSection.jsx
@@ -1,23 +1,25 @@
-import { useEffect, useState } from 'react'
+import { useEffect, useState, useCallback } from 'react'
 import Button from './Button/Button'
 import Modal from './Modal/Modal'
+import useInput from '../hooks/useInput'
 
 export default function EffectSection() {
+    const input = useInput()
     const [modal, setModal] = useState(false)
     const [loading, setLoading] = useState(false)
     const [users, setUsers] = useState([])
 
-    async function fetchUsers() {
+    const fetchUsers = useCallback(async() => {
         setLoading(true)
         const response = await fetch('https://jsonplaceholder.typicode.com/users')
         const users = await response.json()
         setUsers(users)
         setLoading(false)
-    }
-
+    }, [])
+    
     useEffect(() => {
         fetchUsers()
-    }, [])
+    }, [fetchUsers])
 
     return (
         <section>
@@ -37,9 +39,19 @@ export default function EffectSection() {
 
             {loading && <p>Loading...</p>}
 
-            {!loading && <ul>
-                    {users.map(user => <li key={user.id}>{user.name}</li>)}
-                </ul>}
+            {!loading && (
+                <>
+                    <input type="text" className="control" {... input} />
+                    <ul>
+                        {users
+                            .filter((user) => user.name && input.value && user.name.toLowerCase().includes(input.value.toLowerCase()))
+                            .map((user) => (
+                            <li key={user.id}>{user.name}</li>
+                            ))
+                        }
+                    </ul>
+                </>
+            )}
         </section>
     )
 }
